f148ca75f2a09a1f879318642578caf2d0e85b47
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-03-16 11:31:11 -0300

    feat: add PostgresDeleteUserRepository

---
+++ src/repositories/postgres/delete-user.js
@@ -0,0 +1,15 @@
+import { PostgresHelper } from '../../db/postgres/helper.js'
+
+export class PostgresDeleteUser {
+    async execute(userId) {
+        const deleteUser = await PostgresHelper.query(
+            `
+            SELECT * FROM users 
+            WHERE id = $1 
+            RETURNING *
+            `,
+            [userId],
+        )
+        return deleteUser[0]
+    }
+}
