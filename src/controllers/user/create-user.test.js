e44c967d71db90383f1ddd28906decdc428ade94
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-04-05 16:43:00 -0300

    test: ensure 400 is returned if password is less than 6 characters

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -148,4 +148,26 @@ describe('Create User Controller', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if password is less than 6 characters', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'irina',
+                last_name: 'ucrania',
+                email: 'tallin@gmail.com',
+                password: '1234',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+
+        expect(result.statusCode).toBe(400)
+    })
 })
