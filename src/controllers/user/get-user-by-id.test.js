59ee481ae103cff100eb3a0ba07443c8d40a2746
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-04-13 11:12:27 -0300

    feat: ensure GetUserByIdController retuns 400 when an invalid id is provided

--- src/controllers/user/get-user-by-id.test.js
+++ src/controllers/user/get-user-by-id.test.js
@@ -16,6 +16,12 @@ describe('GetUserByIdController', () => {
         }
     }
 
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+    }
+
     const makeSut = () => {
         const getUserByIdUseCase = new GetUserByIdUseCaseStub()
         const sut = new GetUserByIdController(getUserByIdUseCase)
@@ -33,5 +39,42 @@ describe('GetUserByIdController', () => {
         })
         // assert
         expect(result.statusCode).toBe(200)
+    })
+
+    it('should return 400 if a invalid id is provided', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute({ params: { userId: 'invalid_id' } })
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 404 when a user is not found', async () => {
+        // arrange
+        const { sut, getUserByIdUseCase } = makeSut()
+        jest.spyOn(getUserByIdUseCase, 'execute').mockResolvedValue(null)
+
+        // act
+        const result = await sut.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(404)
+    })
+
+    it('should return 500 GetUserByIdUseCase throws', async () => {
+        // arrange
+        const { sut, getUserByIdUseCase } = makeSut()
+        jest.spyOn(getUserByIdUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        // act
+        const result = await sut.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(500)
     })
 })
